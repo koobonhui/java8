4790061e62f35f97360d53692833144d23369c63
koobonhui <koobonhui.dev@gmail.com>
2020-03-16 15:13:56 +0900

    증감

---
+++ java8/src/java8/Day0308.java
@@ -0,0 +1,34 @@
+package java8;
+
+public class Day0308 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 3;
+		
+		a += b;
+		
+		System.out.println(a);
+		
+		a -= b;
+		
+		System.out.println(a);
+		
+		a *= b;
+		
+		System.out.println(a);
+		
+		a = 10;
+		a *= 3 + 10;
+		
+		System.out.println(a);
+		
+		a += 1;
+		
+		System.out.println(a);
+		
+		
+
+	}
+
+}
---
+++ java8/src/java8/Day0309.java
@@ -0,0 +1,56 @@
+package java8;
+
+public class Day0309 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		System.out.println(a);
+		a += 1;
+		System.out.println(a);
+		a += 1;
+		System.out.println(a);
+		a += 1;
+		System.out.println(a);
+		System.out.println("==========");
+		a++;
+		System.out.println(a);
+		a++;
+		System.out.println(a);
+		a++;
+		System.out.println(a);
+		System.out.println("==========");
+		++a;
+		System.out.println(a);
+		++a;
+		System.out.println(a);
+		++a;
+		System.out.println(a);
+		System.out.println("==========");
+		a--;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+		System.out.println("==========");
+		a = 10;
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println("==========");
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println("==========");
+		System.out.println(a);
+		/*
+		int year = 1998, month = 05, date = 25;
+		double we = 63.2, he = 175.5;
+		 */
+	}
+
+}

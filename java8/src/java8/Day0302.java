544c0b67846fdf4f09a75185b3e9e787494d2cf9
koobonhui <koobonhui.dev@gmail.com>
2020-03-16 11:55:33 +0900

    깃허브 하는법

---
+++ java8/src/java8/Day0302.java
@@ -0,0 +1,21 @@
+package java8;
+
+public class Day0302 {
+	public static void main(String[] args) {
+		System.out.println("h");
+		/*
+		 창 보기표시 기타 깃리포지토리
+
+클론 깃허브에 주소복사 아래에 gmail로 로그인 하기
+
+임폴트 체크 완료
+
+리포지토리에 마스터 특성 드가서
+
+user.name
+user.email 추가하기
+		 
+		 */
+	}
+
+}

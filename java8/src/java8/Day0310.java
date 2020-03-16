c6e32176bb9ff9d9374ab36f5bd6915efcaa8ad2
koobonhui <koobonhui.dev@gmail.com>
2020-03-16 15:24:16 +0900

    조건연산

---
+++ java8/src/java8/Day0310.java
@@ -0,0 +1,16 @@
+package java8;
+
+public class Day0310 {
+
+	public static void main(String[] args) {
+		int d = 2_000_000;
+		int g = 1_000_000;
+		
+		String s = d >= g ? "±¤¿ª½Ã" : "½Ã";
+		
+		
+		System.out.println(s);
+
+	}
+
+}

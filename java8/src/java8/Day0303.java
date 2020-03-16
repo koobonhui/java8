d3de769d5be5b26e3143aa60e480bb4afd345d2a
koobonhui <koobonhui.dev@gmail.com>
2020-03-16 12:51:37 +0900

    최대 최소값

---
+++ java8/src/java8/Day0303.java
@@ -0,0 +1,23 @@
+package java8;
+
+public class Day0303 {
+
+	public static void main(String[] args) {
+		/*
+		 byte - Byte
+		 short - Short
+		 int - Integer
+		 long - Long
+		 float - Float
+		 double - Double
+		 char - Character
+	     각각은 max min 존재
+		 */
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+	}
+
+}

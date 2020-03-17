f16ec564ef88b3dddbec7fde29a8f57e3d4e8063
koobonhui <koobonhui.dev@gmail.com>
2020-03-17 14:03:26 +0900

    system.in예

---
+++ java8/src/java8/Day0402.java
@@ -0,0 +1,14 @@
+package java8;
+
+import java.io.IOException;
+
+public class Day0402 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.print("입력해라 : ");
+		int a = System.in.read();
+		System.out.println(a);
+
+	}
+
+}

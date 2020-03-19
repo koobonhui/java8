ca1eab3b7b9e0f9ec6c0a546dfec5c1cac516dd8
koobonhui <koobonhui.dev@gmail.com>
2020-03-19 15:03:09 +0900

    if여러개쓰기

---
+++ java8/src/java8/Day0504.java
@@ -0,0 +1,94 @@
+package java8;
+
+public class Day0504 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		String ok = "";
+		
+		if (num == 1)
+		{
+			ok = "찾음";
+		}
+		else if (num == 2)
+		{
+			ok = "찾음";
+		}
+		else if (num == 3)
+		{
+			ok = "찾음";
+		}
+		else if (num == 4)
+		{
+			ok = "찾음";
+		}
+		else if (num == 5)
+		{
+			ok = "찾음";
+		}
+		else if (num == 6)
+		{
+			ok = "찾음";
+		}
+		else if (num == 7)
+		{
+			ok = "찾음";
+		}
+		else if (num == 8)
+		{
+			ok = "찾음";
+		}
+		else if (num == 9)
+		{
+			ok = "찾음";
+		}
+		else
+		{
+			ok = "찾음";
+		}
+		System.out.println(ok);
+		
+		if (num > 5)
+		{
+			if(num > 7)
+			{
+				if(num > 9)
+				{
+					//10
+				}
+				else
+				{
+					//89
+				}
+				//8910
+			}
+			else
+			{
+				if(num > 6)
+				{
+					//6
+				}
+				else
+				{
+					//7
+				}
+				//67
+			}
+			//678910
+		}
+		else
+		{
+			if(num > 2)
+			{
+				//345
+			}
+			else
+			{
+				//12
+			}
+			//12345
+		}
+
+	}
+
+}

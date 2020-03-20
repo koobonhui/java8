24216ff55516a4272c7879c31dc6e3cb595ffb6a
koobonhui <koobonhui.dev@gmail.com>
2020-03-20 16:24:40 +0900

    continue문

---
+++ java8/src/java8/Day0613.java
@@ -0,0 +1,18 @@
+package java8;
+
+public class Day0613 {
+
+	public static void main(String[] args) {
+		int i;
+		for(i = 1; i <= 10; i++)
+		{
+			if(i % 2 != 0)
+			{
+				continue;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}

53313ab89cb50b4e14f2ae1308af5cdadc6f26a8
koobonhui <koobonhui.dev@gmail.com>
2020-03-20 14:20:45 +0900

    for 구구단

---
+++ java8/src/java8/Day0605.java
@@ -0,0 +1,19 @@
+package java8;
+
+public class Day0605 {
+
+	public static void main(String[] args) {
+		int i, j;
+		for(i = 1; i < 10; i++)
+		{
+			for(j = 2; j < 10; j++)
+			{
+				System.out.print(j + "x" + i + "=" + (i * j) + "\t");
+								
+			}
+			System.out.println();
+		}
+		
+	}
+
+}

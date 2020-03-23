369fb959ff5ffc11fef6370fde8de9aadeb69496
koobonhui <koobonhui.dev@gmail.com>
2020-03-23 09:37:00 +0900

    1~10까지 합

---
+++ java8/src/java8/Day0703.java
@@ -0,0 +1,18 @@
+package java8;
+
+public class Day0703 {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		
+		for(int i = 1; i <= 10; i ++)
+		{
+			sum = sum + i;
+		}
+		
+		System.out.println(sum);
+
+	}
+
+}

60ff1688ba578f1c450822dd1621dfc9eece5188
koobonhui <koobonhui.dev@gmail.com>
2020-03-23 09:44:57 +0900

    1~10까지 짝수 continue문까지

--- java8/src/java8/Day0704.java
+++ java8/src/java8/Day0704.java
@@ -8,9 +8,11 @@ public class Day0704 {
 		
 		for(int i = 1; i <= 10; i ++)
 		{
-			if(i % 2 == 0)
-				sum = sum + i;
+			if(i % 2 != 0)
+				continue;
+			sum = sum + i;
 		}
+		
 		
 		System.out.println(sum);
 

0d9473ce414e0d76dcfdd1f6993e878791d4f83a
koobonhui <koobonhui.dev@gmail.com>
2020-03-24 14:07:19 +0900

    구구단 맞추기 시간나오게

--- java8/src/java8/Day0802.java
+++ java8/src/java8/Day0802.java
@@ -8,22 +8,29 @@ public class Day0802 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Random rd = new Random();
-		int num1 = (int)(Math.random() * 8) + 2;
-		int num2 = rd.nextInt(9) + 1;
 		
-		int quiz = num1 * num2;
+		Long stime = System.currentTimeMillis();
 		
-		System.out.print(num1 + " x " + num2 + " = ");
-		int ans = sc.nextInt();
-		
-		if(quiz == ans)
-		{
-			System.out.println("정답입니다.");
+		int num1, num2, quiz, ans;
+
+		for (int i = 0; i < 5; i++) {
+			num1 = (int) (Math.random() * 8) + 2;
+			num2 = rd.nextInt(9) + 1;
+
+			quiz = num1 * num2;
+
+			System.out.print(num1 + " x " + num2 + " = ");
+			ans = sc.nextInt();
+
+			if (quiz == ans) {
+				System.out.println("정답입니다.");
+			} else {
+				System.out.println("틀렸습니다.");
+				System.exit(0);
+			}
 		}
-		else
-		{
-			System.out.println("틀렸습니다.");
-		}
+		long etime = System.currentTimeMillis();
+		System.out.println(etime - stime + "ms");
 		sc.close();
 	}
 

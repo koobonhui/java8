664ea4b72b8ecfedaac003805b25a62bc8407d21
koobonhui <koobonhui.dev@gmail.com>
2020-03-25 14:41:39 +0900

    @만들기 여러 버전

--- java8/src/java8/Day0901.java
+++ java8/src/java8/Day0901.java
@@ -62,6 +62,115 @@ public class Day0901 {
           System.out.println(""); 
 		}
 		
+		System.out.println("===================");
+		
+		for(int i = 0; i < 5; i++)
+		{
+			for(int j = 0; j < 5; j++)
+			{
+				if(i > j)
+				{
+					System.out.print(" ");
+				}
+				else
+				{
+					System.out.print("@");
+				}
+			}
+			System.out.println();
+		}
+		
+		System.out.println("====================");
+		
+		for (int i = 1; i <= 5; i++) 
+		{ 
+            for (int j = 5 - i; j > 0; j--) 
+            { 
+                  System.out.print(" "); 
+            } 
+            for (int j = 0; j < i; j++) 
+            { 
+                  System.out.print("@"); 
+            }
+            for(int j = 2; j <= i; j++)
+			{
+				System.out.print("@");
+			}
+			System.out.println(); 
+		}
+		
+		System.out.println("=================");
+		
+		for(int i = 4; i >= 0; i--)
+		{
+			for(int j = 0; j < i; j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j = 0; j < 5-i; j++)
+			{
+				System.out.print("@");
+			}
+			for(int j = 0; j < 4-i; j++)
+			{
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("================");
+		
+		for(int i = 0; i < 5; i++)
+		{
+			for(int j = 0; j < i; j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j = 0; j < 5-i; j++)
+			{
+				System.out.print("@");
+			}
+			for(int j = 0; j < 4-i; j++)
+			{
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("================");
+		
+		for(int i = 4; i >= 0; i--)
+		{
+			for(int j = 0; j < i; j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j = 0; j < 5-i; j++)
+			{
+				System.out.print("@");
+			}
+			for(int j = 0; j < 4-i; j++)
+			{
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		for(int i = 0; i < 4; i++)
+		{
+			for(int j = 0; j <= i; j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j = 0; j < 4-i; j++)
+			{
+				System.out.print("@");
+			}
+			for(int j = 0; j < 3-i; j++)
+			{
+				System.out.print("@");
+			}
+			System.out.println();
+		}
 	
 	}
 

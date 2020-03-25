f6aa96bee2fb223bf0448538f9e1e924e0a12bd3
koobonhui <koobonhui.dev@gmail.com>
2020-03-25 15:54:59 +0900

    5개 5줄 씩 출력 + 골뱅이 같은 좌표만 출력

--- java8/src/java8/Day0902.java
+++ java8/src/java8/Day0902.java
@@ -63,6 +63,34 @@ public class Day0902 {
 			}
 			System.out.println();
 		}
+		
+		System.out.println("===========");
+		
+		for(int i = 0; i < 5; i++)
+		{
+			for(int j = 1; j <= i; j++)
+			{
+				System.out.print(" ");
+			}
+			System.out.print("@");
+			System.out.println();
+		}
+		
+		System.out.println("================");
+		
+		for(int i = 0; i < 5; i++)
+		{
+			for(int j = 0; j < 5; j++)
+			{
+				if(i == j)
+				{
+					System.out.print("@");
+				}
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
 	}
 
 }

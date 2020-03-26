1b4a98375d4b301d3e906f9649a0a7b3a33ef96b
koobonhui <koobonhui.dev@gmail.com>
2020-03-26 09:19:40 +0900

    a~z 출력

---
+++ java8/src/java8/Day1001.java
@@ -0,0 +1,18 @@
+package java8;
+
+public class Day1001 {
+
+	public static void main(String[] args) {
+		for(int i = 'a'; i <= 'z'; i++)
+		{
+			System.out.print((char)i + " ");
+		}
+		System.out.println();
+		for(int i = 'A'; i <= 'Z'; i++)
+		{
+			System.out.print((char)i + " ");
+		}
+		System.out.println();
+	}
+
+}

bb0d7d07d2b85d2bd9e68e79a9f9872f3050eaa8
koobonhui <koobonhui.dev@gmail.com>
2020-03-26 11:43:24 +0900

    자료형별 출력

---
+++ java8/src/java8/Day1009.java
@@ -0,0 +1,80 @@
+package java8;
+
+public class Day1009 {
+
+	public static void main(String[] args) {
+			int[] arr = new int[3];
+			
+			for(int i = 0; i < arr.length; i++)
+			{
+				System.out.println(arr[i]);
+			}
+			System.out.println();
+			
+			arr[0] = 10;
+			arr[1] = 20;
+			arr[2] = 30;
+			
+			for(int i = 0; i < arr.length; i++)
+			{
+				System.out.println(arr[i]);
+			}
+			System.out.println();
+			
+			double[] arr2 = new double[3];
+			
+			for(int i = 0; i < arr2.length; i++)
+			{
+				System.out.println(arr2[i]);
+			}
+			System.out.println();
+			
+			arr2[0] = 1.1;
+			arr2[1] = 2.2;
+			arr2[2] = 3.3;
+			
+			for(int i = 0; i < arr2.length; i++)
+			{
+				System.out.println(arr2[i]);
+			}
+			System.out.println();
+			
+			boolean[] arr3 = new boolean[3];
+			
+			for(int i = 0; i < arr3.length; i++)
+			{
+				System.out.println(arr3[i]);
+			}
+			System.out.println();
+			
+			arr3[0] = true;
+			arr3[1] = false;
+			arr3[2] = true;
+			
+			for(int i = 0; i < arr3.length; i++)
+			{
+				System.out.println(arr3[i]);
+			}
+			System.out.println();
+			
+			String[] arr4 = new String[3];
+			
+			for(int i = 0; i < arr4.length; i++)
+			{
+				System.out.println(arr4[i]);
+			}
+			System.out.println();
+			
+			arr4[0] = "°¡";
+			arr4[1] = "³ª";
+			arr4[2] = "´Ù";
+			
+			for(int i = 0; i < arr4.length; i++)
+			{
+				System.out.println(arr4[i]);
+			}
+			System.out.println();
+			
+	}
+
+}

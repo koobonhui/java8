ad0e6b9612ab030299a04df1755dd4325f62721f
koobonhui <koobonhui.dev@gmail.com>
2020-03-30 13:14:44 +0900

    같은 클래스에서 메소드 호출

--- java8/src/Day12/Person.java
+++ java8/src/Day12/Person.java
@@ -1,5 +1,22 @@
 package Day12;
 
 public class Person {
-
+		
+		void sound() {
+			System.out.println("¸»");
+		}
+		
+		void run( ) {
+			System.out.println("´Þ");
+		}
+		
+		void eat() {
+			System.out.println("¸Ô");
+		}
+		
+		void oneday() {
+			sound();
+			run();
+			eat();
+		}
 }
--- java8/src/Day12/Student.java
+++ java8/src/Day12/Student.java
@@ -1,5 +1,13 @@
 package Day12;
 
 public class Student {
-
+	public static void main(String[] args) {
+		Person pe = new Person();
+		
+		pe.eat();
+		pe.sound();
+		pe.run();
+		
+		pe.oneday();
+}
 }

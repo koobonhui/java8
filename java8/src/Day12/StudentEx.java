1bcaffd8238671d4aa1aa1dbd4f9a0a410548062
koobonhui <koobonhui.dev@gmail.com>
2020-03-30 10:08:09 +0900

    클래스 나누기

---
+++ java8/src/Day12/StudentEx.java
@@ -0,0 +1,17 @@
+package Day12;
+
+public class StudentEx {
+
+	public static void main(String[] args) {
+		Student s1 = new Student(); 
+		Person p1 = new Person();
+		
+//		필드 - 클래스에 속성을 저장하는것을 필드라함 (변수는 필드에 들어감)
+//		
+//		생성자 - 클래스를 객체로 만들때(인스턴스화) 한번 실행되는 영역
+//		
+//		메소드 - 행위 이 클래스가 어떤 동작을 하는지 나열한 곳 (변수 뒤에 ()이게 있으면 메소드 아니면 필드) 
+		
+	}
+
+}

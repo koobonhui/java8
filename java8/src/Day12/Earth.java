4eb20f60a3499d76e33dd8513dd9665d9e4b604c
koobonhui <koobonhui.dev@gmail.com>
2020-03-30 14:06:25 +0900

    static final 쓰는법

---
+++ java8/src/Day12/Earth.java
@@ -0,0 +1,13 @@
+package Day12;
+
+public class Earth {
+	
+	static final double RADIUS = 6400;
+	static final double SURFACE = 4 * Math.PI * RADIUS * RADIUS;;
+	
+//	static {
+//		SURFACE = 4 * Math.PI * RADIUS * RADIUS;
+//	}
+	
+
+}
---
+++ java8/src/Day12/EarthEx.java
@@ -0,0 +1,13 @@
+package Day12;
+
+public class EarthEx {
+
+	public static void main(String[] args) {
+		
+			System.out.println("지구 반지름 : " + Earth.RADIUS);
+			
+			System.out.println("지구 표 면적 : " + Earth.SURFACE);
+
+	}
+
+}

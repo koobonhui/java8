0a9c17b621a310d4b1114dd1db3bacc4f151d286
koobonhui <koobonhui.dev@gmail.com>
2020-03-31 14:54:42 +0900

    CompareTo 사용 (수 비교할때 씀)

---
+++ java8/src/Day13/CompareEx.java
@@ -0,0 +1,15 @@
+package Day13;
+
+public class CompareEx {
+
+	public static void main(String[] args) {
+		String a = "aaa";
+		String b = "abc";
+		
+		int res = a.compareTo(b);
+		
+		System.out.println(res);
+
+	}
+
+}

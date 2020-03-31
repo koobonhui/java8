ca894280f93d8113d9b1371d144eb100d7ecee61
koobonhui <koobonhui.dev@gmail.com>
2020-03-31 10:45:28 +0900

    오버로딩 예

---
+++ java8/src/Day13/Printer.java
@@ -0,0 +1,21 @@
+package Day13;
+
+public class Printer {
+	
+		void println(int arg) {
+			System.out.println(arg);
+		}
+		
+		void println(boolean arg) {
+			System.out.println(arg);
+		}
+		
+		void println(double arg) {
+			System.out.println(arg);
+		}
+		
+		void println(String arg) {
+			System.out.println(arg);
+		}
+
+}
---
+++ java8/src/Day13/PrinterEx.java
@@ -0,0 +1,15 @@
+package Day13;
+
+public class PrinterEx {
+
+	public static void main(String[] args) {
+		Printer p = new Printer();
+		
+		p.println(10);
+		p.println(true);
+		p.println(3.14);
+		p.println("Hello");
+
+	}
+
+}

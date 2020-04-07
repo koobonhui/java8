30dfe03c17f6f245a4ae4e93672f5f692223c424
koobonhui <koobonhui.dev@gmail.com>
2020-04-07 13:29:26 +0900

    람다식

---
+++ java8/src/Day18/Functional.java
@@ -0,0 +1,9 @@
+package Day18;
+
+@FunctionalInterface
+public interface Functional {
+		void method();
+//		void method2();            람다식으로 만들면 메소드는 1개만 있어야한다 이말이야
+//		int method3();				꼭 적어야하는건 아니지만 알면 좋음
+
+}
---
+++ java8/src/Day18/FunctionalEx.java
@@ -0,0 +1,27 @@
+package Day18;
+
+public class FunctionalEx{
+
+	public static void main(String[] args) {
+		Functional f1 = new Functional() {
+			
+			@Override
+			public void method() {
+				System.out.println("f1 익명구현객체로 메소드 호출");
+			}
+		};
+
+		f1.method();
+		
+		Functional f2 = () -> {
+			System.out.println("f2 람다식으로 메소드 호출");
+		};
+		
+		f2.method();
+		
+		Functional f3 = () -> System.out.println("f3 람다식으로 메소드 호출");
+		// 중괄호 영역이 한줄이면 {};삭제해도 된다
+		f3.method();
+	}
+
+}

7e24ca6dabae177d4204d32ccd8da9c764b7e6ad
koobonhui <koobonhui.dev@gmail.com>
2020-04-09 10:44:00 +0900

    Join() 스레드

---
+++ java8/src/Day20/JoinEx.java
@@ -0,0 +1,13 @@
+package Day20;
+
+public class JoinEx {
+	public static void main(String[] args) {
+			SumThread t = new SumThread();
+			t.start();
+			try {
+				Thread.sleep(10);
+				t.join();
+			} catch (InterruptedException e) {}
+			System.out.println(t.getSum());
+		}
+}

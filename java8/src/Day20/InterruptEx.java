dab7fac1bfc75add6c692e81f1246920c986d40c
koobonhui <koobonhui.dev@gmail.com>
2020-04-09 11:35:28 +0900

    Interrupted()를 사용한 스레드 종료

---
+++ java8/src/Day20/InterruptEx.java
@@ -0,0 +1,19 @@
+package Day20;
+
+public class InterruptEx {
+
+	public static void main(String[] args) {
+		
+		Thread thread = new PrintThread2();
+		
+		thread.start();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		}
+		
+		thread.interrupt();
+	}
+
+}
